d35524b95881ab89fc0f460901fd8fc0244bc772
Aytac G <aytacgenc1@gmail.com>
2023-08-13 23:27:38 +0300

    try-catch

---
+++ vars/try_catch.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+
+def call() {
+      try {
+         def arr = new int[3];
+         arr[5] = 5;
+      } catch(Exception ex) {
+         echo ":.. Echo message..: "
+         echo ex.getMessage
+         buildResult = currentBuild.result
+         currentBuild.result = "UNSTABLE"
+      }
+}
+
