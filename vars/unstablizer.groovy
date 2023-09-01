4e4f38beb5dd3375c3f1efed90508443e4d88aeb
Aytac G <aytacgenc1@gmail.com>
2023-09-01 13:09:57 +0300

    script within groovy

--- vars/unstablizer.groovy
+++ vars/unstablizer.groovy
@@ -4,5 +4,8 @@ def call() {
   echo "destabilazing the middle east..."
   buildResult = currentBuild.result
   currentBuild.result = "UNSTABLE"
+  script {
+    sayHello('To the new script info')
+  }
 }
 
