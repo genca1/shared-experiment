6433517fc5e2d345414ac1307fb2183db4dc1d7b
Aytac G <aytacgenc1@gmail.com>
2023-09-03 18:34:02 +0300

    string assigment

--- vars/commandRun.groovy
+++ vars/commandRun.groovy
@@ -1,10 +1,13 @@
 def call(def command) {
     String ab = 'AAA';
+    String af;
 
     echo "booom ${ab}";
 
     ab = 'BBBB';
+    af = 'wewew';
 
     echo "changed to??? ${ab}"
+    echo "assinged to??? ${af}"
     echo runner(command)
 }
