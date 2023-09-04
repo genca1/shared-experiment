9c1034bcfc1f380d0375b278af112fdc82750460
Aytac G <aytacgenc1@gmail.com>
2023-09-04 10:04:44 +0300

    typo

--- vars/secretCred.groovy
+++ vars/secretCred.groovy
@@ -1,4 +1,4 @@
-def call(username) {
+def call(def username) {
     def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
             com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
             jenkins.model.Jenkins.instance
