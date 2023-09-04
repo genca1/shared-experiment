5108c7f4cef63bf9c6391876b854396bde1e9c79
Aytac G <aytacgenc1@gmail.com>
2023-09-04 09:19:38 +0300

    username getter

--- vars/usernameGetter.groovy
+++ vars/usernameGetter.groovy
@@ -2,20 +2,20 @@ import jenkins.*
 import jenkins.model.* 
 import hudson.*
 import hudson.model.*
-def jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-        com.cloudbees.plugins.credentials.Credentials.class,
-        Jenkins.instance,
-        null,
-        null
-);
-
 def call(usernameId) {
+    def jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+            com.cloudbees.plugins.credentials.Credentials.class,
+            Jenkins.instance,
+            null,
+            null
+    );
+
     for (creds in jenkinsCredentials) {
-        println(creds.id)
-        if(creds.id == usernameId) {
-            println('bulunduuuu..: ' + creds.id)
-        } else {
-            println('bulunamadi')
-        }
+            println(creds.id)
+            if(creds.id == usernameId) {
+                println('bulunduuuu..: ' + creds.id)
+            } else {
+                println('bulunamadi')
+            }
     }
 }
